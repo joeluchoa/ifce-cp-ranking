aab21d694c983579bca78584c08705329ec02e4e
Joel Uchoa <joeluchoa@Joels-MacBook-Pro.local>
2019-01-21 17:58:10 -0300

    Testando codeforces API

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import requests
+
+def data():
+    resp = requests.get('http://codeforces.com/api/user.info?handles=tsebayoth;KAN;Um_nik;isaf27')
+    if resp.status_code != 200:
+        raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+    return resp.json()
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    d = data()
+    return render_template('home.html', data = d)
 
 if __name__ == '__main__':
     app.run(debug = True)
